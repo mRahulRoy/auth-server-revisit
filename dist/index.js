9ec92f8b68bb3343d7cfeb79d99634e0cff3f6e2
rahul roy <rahul.roy@thewasserstoff.com>
2025-08-18 23:11:42 +0530

    add typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function name(name) {
+    console.log("Hello world", name);
+}
+name("Rahul");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function name(name: string) {
+  console.log("Hello world", name);
+}
+
+name("Rahul");
